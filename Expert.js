4b515569fba7248bb8fe87681bf86e883a5f336f
oguzhan cicek <oguzhancicek01@hotmail.com>
2024-11-14 13:26:48 +0300

    first commit

---
+++ Expert.js
@@ -0,0 +1,36 @@
+function myfunction1(){
+    let here = document.getElementById("londonaction");
+        here.classList.remove("ghost")
+   
+    let here2 = document.getElementById("parisaction");
+        here2.classList.add("ghost")
+
+    let here3 = document.getElementById("tokyoaction");
+        here3.classList.add("ghost")
+}
+
+
+
+
+function myfunction2(){
+    let here = document.getElementById("londonaction");
+    here.classList.add("ghost")
+
+    let here2 = document.getElementById("parisaction");
+        here2.classList.remove("ghost")
+
+    let here3 = document.getElementById("tokyoaction");
+        here3.classList.add("ghost")    
+}
+
+function myfunction3(){
+    let here = document.getElementById("londonaction");
+    here.classList.add("ghost")
+
+    let here2 = document.getElementById("tokyoaction");
+        here2.classList.remove("ghost")
+
+    let here3 = document.getElementById("parisaction");
+        here3.classList.add("ghost")
+}
+
