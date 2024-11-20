d7d64cc4d77ef4119b9037c1b64571c0927ea4af
oguzhan cicek <oguzhancicek01@hotmail.com>
2024-11-20 20:09:46 +0300

    update 2

--- Expert.js
+++ Expert.js
@@ -7,6 +7,16 @@ function myfunction1(){
 
     let here3 = document.getElementById("tokyoaction");
         here3.classList.add("ghost")
+
+
+    const select = document.getElementById("london12");
+        select.classList.add("selected1")
+
+     const select1 = document.getElementById("paris12");
+         select1.classList.remove("selected2")
+
+     const select2 = document.getElementById("tokyo12");
+         select2.classList.remove("selected3")
 }
 
 
@@ -20,8 +30,19 @@ function myfunction2(){
         here2.classList.remove("ghost")
 
     let here3 = document.getElementById("tokyoaction");
-        here3.classList.add("ghost")    
+        here3.classList.add("ghost")
+    
+        
+    const select = document.getElementById("paris12");
+        select.classList.add("selected2")
+
+    const select1 = document.getElementById("london12");
+        select1.classList.remove("selected1")
+
+    const select2 = document.getElementById("tokyo12");
+         select2.classList.remove("selected3")
 }
+
 
 function myfunction3(){
     let here = document.getElementById("londonaction");
@@ -32,5 +53,15 @@ function myfunction3(){
 
     let here3 = document.getElementById("parisaction");
         here3.classList.add("ghost")
+        
+
+    const select = document.getElementById("tokyo12");
+        select.classList.add("selected3")
+
+    const select1 = document.getElementById("paris12");
+        select1.classList.remove("selected2")
+
+    const select2 = document.getElementById("london12");
+        select2.classList.remove("selected1")
 }
 
